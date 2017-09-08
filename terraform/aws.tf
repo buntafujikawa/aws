fa7c98443af78ee1fdd1360c45a7f5e1bd2ca6f6
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2017-09-08 23:22:08 +0900

    ec2を最低限の設定で立ち上げる

---
+++ terraform/aws.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+    default = "ap-northeast-1"
+}
+
+variable "aws_zone" {
+    default = "ap-northeast-1c"
+}
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region     = "${var.aws_region}"
+}
