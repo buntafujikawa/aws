2abe38d0d6e3ffa63ac1b49b4d503fc9433bcb3d
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2017-09-30 01:52:49 +0900

    重複分を削除

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,15 +1,15 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {
-    default = "ap-northeast-1"
+  default = "ap-northeast-1"
 }
 
 variable "aws_zone" {
-    default = "ap-northeast-1c"
+  default = "ap-northeast-1c"
 }
 
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region     = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
 }
