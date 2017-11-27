8a34b1444dfc14eb3bf3776b5e061e7af7709d7c
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2017-11-27 22:58:39 +0900

    terraformingを使って差分を追加

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,23 +1,65 @@
-resource "aws_instance" "sitename-web1" {
+resource "aws_instance" "sitename-production1" {
   ami                         = "ami-374db956" # これちょっと古い
+  availability_zone           = "ap-northeast-1a"
+  ebs_optimized               = false
   instance_type               = "t2.micro"
-  key_name                    = "ec2_test_keypair" # 自分の環境で作成したkeypairの名前に変更
-  vpc_security_group_ids      = ["${aws_security_group.app-sg.id}"]
-  subnet_id                   = "${aws_subnet.public-web.id}"
-  associate_public_ip_address = true
   monitoring                  = true
-  disable_api_termination     = false # 間違って削除をしない設定、重要なインスタンスはtrueにしておいたほうが良い
+  key_name                    = "ec2_test_keypair"
+  subnet_id                   = "${aws_subnet.public-web.id}"
+  vpc_security_group_ids      = ["${aws_security_group.app-sg.id}"]
+  associate_public_ip_address = true
+  private_ip                  = "10.0.0.84"
+  source_dest_check           = true
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+    delete_on_termination = true
+  }
 
   tags {
-    Name = "sitename_web1"
+    "Name" = "sitename-production1"
   }
 }
 
-resource "aws_eip" "web1-eip" {
-  instance = "${aws_instance.sitename-web1.id}"
+resource "aws_instance" "sitename-staging" {
+  ami                         = "ami-2a69be4c"
+  availability_zone           = "ap-northeast-1c"
+  ebs_optimized               = false
+  instance_type               = "t2.micro"
+  monitoring                  = true
+  key_name                    = "ec2_test_keypair"
+  subnet_id                   = "${aws_subnet.public-web-staging.id}"
+  vpc_security_group_ids      = ["${aws_security_group.app-sg.id}"]
+  associate_public_ip_address = true
+  private_ip                  = "10.0.1.24"
+  source_dest_check           = true
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+    delete_on_termination = true
+  }
+
+  tags {
+    "Name" = "sitename-staging"
+  }
+}
+
+resource "aws_eip" "sitename-production1-eip" {
+  instance = "${aws_instance.sitename-production1.id}"
   vpc      = true
 }
 
-output "elastic_ip_of_web" {
-  value = "${aws_eip.web1-eip.public_ip}"
+resource "aws_eip" "sitename-staging-eip" {
+  instance = "${aws_instance.sitename-staging.id}"
+  vpc      = true
+}
+
+output "elastic_ip_of_production" {
+  value = "${aws_eip.sitename-production1-eip.public_ip}"
+}
+
+output "elastic_ip_of_staging" {
+  value = "${aws_eip.sitename-staging-eip.public_ip}"
 }
